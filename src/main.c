57edda11d4afde97dc405b6e865240f376c38b34
Benjamin Roberts <Benjamin@BGRoberts.id.au>
2014-03-03 21:13:00 +1100

    basic read-eval loop with vfork/execlp

--- src/main.c
+++ src/main.c
@@ -2,8 +2,22 @@
     Benjamin Roberts 2014
     COMP2300 Assignment One */
 
+#include<unistd.h>
+#include<stdio.h>
+#include<sys/types.h>
+#include "jobs.c"
         
 int main(int argc, char * argv[])
 {
+    char input_buffer[80];
+    
+    while(scanf("%s", input_buffer)) {
+        pid_t pid = vfork();
+        if(pid == 0) {
+            execlp(input_buffer, input_buffer, (char *) 0);
+        } else if (pid < 0) {
+            return -1;
+        } 
+    }
     return 0;
 } 
